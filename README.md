67d5218459c6ba7fc3b128ab42757eaac30a1aac
Ilir Vejsa <vejsa.ilir@hotmail.com>
2025-09-16 21:04:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# 💪 CoreGymUI
+
+> Vi lyfter kod, inte vikter.
+
+## 🏋️‍♀️ Välkommen till CoreGymUI
+
+Detta är vårt frontend-projekt byggt med **React + Vite**.  
+Här tränar vi inte kroppen – vi tränar **komponenter**, **hooks** och **agila muskler**.
+
+## 🚀 Kom igång
+
+1. Klona repot (du är redan här 😉)
+2. Glöm inte skapa era branch!
+
+```bash
+npm install
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '5rem' }}>
+            <h1>💪 CoreGymUI 💪</h1>
+            <p>Vi lyfter kod, inte vikter!</p>
+            <img
+                src="https://media.giphy.com/media/3o6Zt481isNVuQI1l6/giphy.gif"
+                alt="funny gym gif"
+                width="300"
+            />
+        </div>
+    );
+}
+
+export default App;

97c01b63f402609a714d5a84da1e2624889f17fe
rdunder <r.dunder@gmail.com>
2025-09-18 13:10:55 +0200

    changed name of the target on logo link

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,7 +5,7 @@ import './logo.css'
 export default function Logo() {
   return (
     // Change <a> to be Link to route /home later
-    <a className='header-logo-container' href='#'>
+    <a className='header-logo-container' href='#Home'>
         <img className='header-logo' src={logo} alt='logotype'/>
     </a>
   )

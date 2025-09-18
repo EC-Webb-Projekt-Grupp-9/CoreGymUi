f5271de3a3bbd0e0ade217689cda0bca675c78ce
rdunder <r.dunder@gmail.com>
2025-09-18 11:16:53 +0200

    Basic styles added to header and Logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Logo from '../Logo/Logo'
+import './Header.css'
 
 export default function Header() {
   return (
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import logo from '../../assets/images/Logo-Core 1.svg'
+import './logo.css'
 
 export default function Logo() {
   return (
     // Change <a> to be Link to route /home later
-    <a>
-        <img src={logo} alt='logotype'/>
+    <a className='header-logo-container'>
+        <img className='header-logo' src={logo} alt='logotype'/>
     </a>
   )
 }

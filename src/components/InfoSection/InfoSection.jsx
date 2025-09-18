b715a5cc140518e08f3278ba0e2b34e43883797f
Ridah Omran <ridah.omran@learnet.se>
2025-09-18 15:17:54 +0200

    Fixed InfoSection.jsx, as there was conflict after two commits on the same .jsx file

--- src/components/InfoSection/InfoSection.jsx
+++ src/components/InfoSection/InfoSection.jsx
@@ -3,11 +3,21 @@ import './InfoSection.css';
 import InfoImage from '../../assets/images/Info-image.svg'
 
 const InfoSection = () => {
-    return (
-        <section className="info-section">
-            <img src={InfoImage} alt="Gym info" className="gym-info" />
-        </section>
-    );
-};
+  return (
+    <div className="info-section">
+        <div className="info-left-side">
+            <img src={InfoImage} alt="InfoImage" />
+        </div>
+        <div className="info-right-side">
+            <div className="gym-info">
+                <h2 className="gym-info-title">Information om gymmet</h2>
+                <p className="gym-info-text">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
+                </p>    
+            </div>
+        </div>
+    </div>
+  )
+}
 
-export default InfoSection;
+export default InfoSection

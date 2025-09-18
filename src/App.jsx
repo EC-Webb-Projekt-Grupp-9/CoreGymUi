a6d49002bb0c66b5aea7dcc405c361459a31381e
Ilir Vejsa <vejsa.ilir@hotmail.com>
2025-09-18 14:47:47 +0200

    Rensade InfoSection, behåller bara ContactSection för min sprint

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './components/Header/Header';
 import Index from './components/Pages/Index';
 import InfoSection from './components/InfoSection/InfoSection';
+import ContactSection from './components/Contact/ContactSection';
 
 function App() {
     return (
         <>
             <Header />
             <Index />
+            <ContactSection />
             <InfoSection />
         </>
     );

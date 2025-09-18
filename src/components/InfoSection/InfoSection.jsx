08c17bf7971e1d793a9c787454762dd37b796443
Ridah Omran <ridah.omran@learnet.se>
2025-09-18 14:16:19 +0200

    Added < InfoSection /> to Index.jsx, then added HTML elements to InfoSection.jsx, including image and text,  with some basic CSS styling

---
+++ src/components/InfoSection/InfoSection.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import InfoImage from '../../assets/images/Info-image.svg'
+import './InfoSection.css'
+
+const InfoSection = () => {
+  return (
+    <div className="info-section">
+        <div className="info-left-side">
+            <img src={InfoImage} alt="InfoImage" />
+        </div>
+        <div className="info-right-side">
+            <div className="gym-info">
+                <h2 className="gym-info-title">Information om gymmet</h2>
+                <p className="gym-info-text">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
+                </p>    
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default InfoSection

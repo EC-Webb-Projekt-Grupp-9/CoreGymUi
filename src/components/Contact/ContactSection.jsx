c4fb427e8812aa5a8fa0944a30b00ea41b2bc8bb
xjosob <jocke.s@windowslive.com>
2025-09-18 15:58:55 +0200

    Added map to ContactSection

--- src/components/Contact/ContactSection.jsx
+++ src/components/Contact/ContactSection.jsx
@@ -1,65 +1,68 @@
-﻿import React from 'react';
-import './ContactSection.css';
+﻿import React from "react";
+import "./ContactSection.css";
 
 const ContactSection = () => {
-    return (
-        <section className="contact-section">
-            <div className="container">
-                <div className="row">
-                    {/* Contact Information */}
-                    <div className="col-lg-6">
-                        <div className="contact-info">
-                            {/* Address Info */}
-                            <div className="address-section">
-                                <h5 className="contact-heading">Berzeliisgatan 18</h5>
-                                <p className="mb-1">412 53 Göteborg</p>
-                                <p className="mb-1">Tel: 070 1234 5678</p>
-                                <p className="mb-0">E-Mail: boka@core-gym.com</p>
-                            </div>
+  return (
+    <section className="contact-section">
+      <div className="container">
+        <div className="row">
+          {/* Contact Information */}
+          <div className="col-lg-6">
+            <div className="contact-info">
+              {/* Address Info */}
+              <div className="address-section">
+                <h5 className="contact-heading">Berzeliisgatan 18</h5>
+                <p className="mb-1">412 53 Göteborg</p>
+                <p className="mb-1">Tel: 070 1234 5678</p>
+                <p className="mb-0">E-Mail: boka@core-gym.com</p>
+              </div>
 
-                            {/* Opening Hours - FOKUS PÅ DENNA DEL FÖR USER STORY */}
-                            <div className="opening-hours-section">
-                                <h3 className="opening-hours-title">Öppettider</h3>
-                                <div className="opening-hours-content">
-                                    <div className="hours-row">
-                                        <span className="days">Mån - Fre:</span>
-                                        <span className="times">07:00 - 21:00</span>
-                                    </div>
-                                    <div className="hours-row">
-                                        <span className="days">Lör - Sön:</span>
-                                        <span className="times">10:00 - 15:00</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+              {/* Opening Hours - FOKUS PÅ DENNA DEL FÖR USER STORY */}
+              <div className="opening-hours-section">
+                <h3 className="opening-hours-title">Öppettider</h3>
+                <div className="opening-hours-content">
+                  <div className="hours-row">
+                    <span className="days">Mån - Fre:</span>
+                    <span className="times">07:00 - 21:00</span>
+                  </div>
+                  <div className="hours-row">
+                    <span className="days">Lör - Sön:</span>
+                    <span className="times">10:00 - 15:00</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
 
-                    {/* Map */}
-                    <div className="col-lg-6">
-                        <div className="map-container">
-                            {/* Ersätt med din karta */}
-                            <div className="map-placeholder">
-                                <p className="text-center text-muted">
-                                    LÄGG IN KARTA HÄR
-                                    <br />
-                                    <small>(Google Maps embed)</small>
-                                </p>
-                            </div>
-                            {/* När du har kartan, använd detta istället:
+          {/* Map */}
+          <div className="col-lg-6">
+            <div className="map-container">
+              {/* Ersätt med din karta */}
+              <div className="map-container">
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2133.2902399406723!2d11.979402!3d57.698198!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464ff36dcb12345%3A0x123456789abcdef!2sBerzeliigatan%2018%2C%20412%2053%20Göteborg!5e0!3m2!1ssv!2sse!4v1694543255555!5m2!1ssv!2sse"
+                  className="map-embed"
+                  allowFullScreen=""
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
+              </div>
+
+              {/* När du har kartan, använd detta istället:
               <iframe
                 src="https://www.google.com/maps/embed?pb=YOUR_EMBED_URL_HERE"
                 className="map-embed"
                 allowFullScreen=""
                 loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade">
-              </iframe>
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
               */}
-                        </div>
-                    </div>
-                </div>
             </div>
-        </section>
-    );
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ContactSection;

4bbadd50ee7867ef82a8884531667d3598880b94
xjosob <jocke.s@windowslive.com>
2025-09-19 13:22:57 +0200

    Removed duplicate Index import in App and added back ContactSection to Index page

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom'
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 
-import Header from './components/Header/Header';
-import Index from './components/Pages/Index'
-import Booking from './components/Pages/Booking'
-import Index from './components/Pages/Index';
-import InfoSection from './components/InfoSection/InfoSection';
-import ContactSection from './components/Contact/ContactSection';
-
+import Header from "./components/Header/Header";
+import Index from "./components/Pages/Index";
+import Booking from "./components/Pages/Booking";
+import InfoSection from "./components/InfoSection/InfoSection";
+import ContactSection from "./components/Contact/ContactSection";
 
 function App() {
-    return (
-
-        <main>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Index />} />
-                <Route path="/booking" element={<Booking />} />          
-            </Routes>
-        </main>
-
-            
-    );
+  return (
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/booking" element={<Booking />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;

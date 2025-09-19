6bb7f6d844b85ef8c19f33a0e90189d065f85fd9
rdunder <r.dunder@gmail.com>
2025-09-19 13:48:09 +0200

    remove dupicates and not used imports from app.jsx, NO BARNCH WAS CREATED

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,10 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom'
 import './App.css';
-import Header from './components/Header/Header';
-import Booking from './components/Pages/Booking'
-import InfoSection from './components/InfoSection/InfoSection';
-import ContactSection from './components/Contact/ContactSection';
 
 import Header from "./components/Header/Header";
 import Index from "./components/Pages/Index";
 import Booking from "./components/Pages/Booking";
-import InfoSection from "./components/InfoSection/InfoSection";
-import ContactSection from "./components/Contact/ContactSection";
 
 function App() {
   return (

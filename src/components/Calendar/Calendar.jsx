12faae1c929344a1f8b25d8dae80afea6b0dbbfe
Dennis Hed Rudin <Dennishr1994@hotmail.com>
2025-09-24 11:37:15 +0200

    visar endast 7 dagar med pilar

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -5,23 +5,31 @@ export default function Calendar({pickedDateChanged, dateObjects}) {
     const [pickedDay, setPickedDay] = useState("")
     const [today, setToday] = useState("")
     const [isMobile, setIsMobile] = useState(false)
+    const [displayDates, setDisplayDates] = useState([])
 
     useEffect( () => {
         getTodaysDate()
         checkScreenSize()
+        setDisplayDates(isMobile ? dateObjects.filter(date => date.getDate() >= today.getDate()).slice(0, 3) : dateObjects.filter(date => date.getDate() >= today.getDate()).slice(0,7))         
 
         // Lyssna på fönsterstorlek ändringar
         const handleResize = () => {
             checkScreenSize()
         }
 
-        window.addEventListener('resize', handleResize)
+        window.addEventListener('resize', handleResize)        
         return () => window.removeEventListener('resize', handleResize)
-    }, [pickedDay])
+       
+    }, [pickedDay, dateObjects, isMobile])
 
     function checkScreenSize() {
         setIsMobile(window.innerWidth <= 768)
     }
+
+    function showNextPeriod() {
+        console.log('hej hej')
+    }
+
 
     function handlePickDay(date) {
         pickedDateChanged(date)
@@ -34,16 +42,22 @@ export default function Calendar({pickedDateChanged, dateObjects}) {
         console.log(today)
     }
 
+
     function isSameDate(date1, date2) {
         if (!date1 || !date2) return false;
         return date1.getDate() === date2.getDate()
     }
 
+    
+
+
+
     // Visa bara första 3 dagarna på mobil, alla på desktop
-    const displayDates = isMobile ? dateObjects.slice(0, 3) : dateObjects
+    
 
     return (
         <div className='calendar-container'>
+            <i className="bi bi-arrow-left-short" onClick={showNextPeriod}></i>
             {displayDates.map( (date, index) => (
                 <div
                     className={`day ${date.toString() === pickedDay.toString() ? "picked" : ""} ${isSameDate(date, today) ? "today" : ""}`}
@@ -64,6 +78,9 @@ export default function Calendar({pickedDateChanged, dateObjects}) {
 
                 </div>
             )}
+            <i className="bi bi-arrow-right-short" onClick={showNextPeriod}></i>            
         </div>
+
+    
     )
 }

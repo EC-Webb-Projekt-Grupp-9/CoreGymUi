90f44ef554579191482caf3df31295037bc1bcb9
xjosob <jocke.s@windowslive.com>
2025-09-24 13:52:10 +0200

    Fixed navbar-toggler color

--- src/components/Hamburger/Hamburger.jsx
+++ src/components/Hamburger/Hamburger.jsx
@@ -1,25 +1,37 @@
-import React from 'react'
-import { Links, NavLink } from 'react-router-dom'
-import './hamburger.css'
+import React from "react";
+import { Links, NavLink } from "react-router-dom";
+import "./hamburger.css";
 
 export default function Hamburger() {
   return (
-    <nav class="navbar navbar-expand-lg ">
-  <div class="container-fluid">    
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item" >
-          <NavLink to={"/"}  className='nav-links'>Startsida</NavLink>        
-        </li>
-        <li class="nav-item" >
-          <NavLink to={"/booking"}  className='nav-links'>Boka Pass</NavLink>
-        </li>        
-      </ul>
-    </div>
-  </div>
-</nav>
-  )
+    <nav className="navbar navbar-expand-lg ">
+      <div class="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <NavLink to={"/"} className="nav-links">
+                Startsida
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to={"/booking"} className="nav-links">
+                Boka Pass
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
